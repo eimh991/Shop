3607373d92d0c5d7e0408bb126c8e47b0692b4e0
unknown <lecoqsportif10@mail.ru>
2024-09-12 13:42:10 +0400

    Add IRepository interfaces and UserRepository

---
+++ Shop/Repositories/UserRepository.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Shop.Data;
+using Shop.Interfaces;
+using Shop.Model;
+
+namespace Shop.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public  async Task CreateAsync(User entity)
+        {
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == id);
+            
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+
+                await _context.SaveChangesAsync();
+            }
+            //как альтернатива
+            //await _context.Users.Where(u=>u.UserId == id).ExecuteDeleteAsync();
+
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            var users = await _context.Users
+                .AsNoTracking()
+                .Include(u=>u.Orders)
+                .ToListAsync();
+
+            if (users != null)
+            {
+                return users;
+            }
+            return new List<User>() ;
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var user = await _context.Users
+                .AsNoTracking()
+                .Include(u=>u.Orders)
+                .FirstOrDefaultAsync(u => u.UserId == id);
+
+            if (user != null)
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            await _context.Users
+                .Where(u => u.UserId == entity.UserId)
+                .ExecuteUpdateAsync(s =>s
+                    .SetProperty(u => u.UserId, entity.UserId)
+                    .SetProperty(u => u.UserName, entity.UserName)
+                    .SetProperty(u => u.Email, entity.Email)
+                    .SetProperty(u => u.PasswordHash, entity.PasswordHash)
+                    .SetProperty(u => u.Balance, entity.Balance)
+                    .SetProperty(u => u.Role, entity.Role)
+                );
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            var user = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email);
+
+            if (user != null)
+            {
+                return user;
+            }
+            return null;
+        }
+    }
+}
