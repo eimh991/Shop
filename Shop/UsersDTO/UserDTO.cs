6c29382294986220a5a82bc41c1f17df88a49607
unknown <lecoqsportif10@mail.ru>
2024-09-24 20:59:48 +0400

    delete Directory -UserDTO

--- Shop/UsersDTO/UserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shop.UsersDTO
-{
-    public class UserDTO
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-    }
-}
