deca8d23f463a2c93c60298bf6db7866109bf75b
unknown <lecoqsportif10@mail.ru>
2024-10-12 23:02:37 +0400

    Добавил Интерфейс и сервис для создания и получение заказа, создание просто берет все итемы которые есть в корзине и премещает их в заказ а корзина очищается

---
+++ Shop/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Model;
+
+namespace Shop.Interfaces
+{
+    public interface IOrderService
+    {
+        public Task CreateOrderAsync(int userId, IEnumerable<CartItem> items);
+        public Task<Order> GetOrderById(int userId, int entityId);
+
+        public Task<IEnumerable<Order>> GetAllOrders(int userId); 
+    }
+}
---
+++ Shop/Service/OrderService.cs
@@ -0,0 +1,64 @@
+﻿using Shop.Interfaces;
+using Shop.Model;
+using Shop.Repositories;
+
+namespace Shop.Service
+{
+    public class OrderService : IOrderService
+    {
+
+        private readonly IRepositoryWithUser<Order> _orderRepository;
+        private readonly IRepositoryWithUser<CartItem> _cartItemRepository;
+
+        public OrderService(IRepositoryWithUser<Order> orderRepository, IRepositoryWithUser<CartItem> cartItemRepository)
+        {
+            _orderRepository = orderRepository;
+            _cartItemRepository = cartItemRepository;
+        }
+
+        public async Task CreateOrderAsync(int userId,IEnumerable<CartItem> items)
+        {
+            var orderItems = ConvertCartItemsToOrderItems(items);
+            var order = new Order
+            {
+                OrderItems = orderItems.ToList(),
+                OrderDate = DateTime.Now,
+                TotalAmount = AllOrderPrice(orderItems.ToList())
+            };
+            await _orderRepository.AddAsync(userId, order);
+            await ((CartItemRepository)_cartItemRepository).DeleteAllCartItemsAsync(userId);
+        }
+
+        public async Task<IEnumerable<Order>> GetAllOrders(int userId)
+        {
+           return  await _orderRepository.GetAllAsync(userId)?? 
+                Enumerable.Empty<Order>(); ;
+     
+        }
+
+        public async Task<Order> GetOrderById(int userId, int entityId)
+        {
+            return await GetOrderById(userId, entityId);
+        }
+
+        private IEnumerable<OrderItem> ConvertCartItemsToOrderItems(IEnumerable<CartItem> items)
+        {
+            return items.Select(i => new OrderItem
+            {
+                Product = i.Product,
+                Quantity = i.Quantity,
+                UnitPrice = i.Quantity * i.Product.Price,
+            });
+        }
+
+        private decimal AllOrderPrice(List<OrderItem> orderItems)
+        {
+            decimal allPrice = 0;
+            for (int i = 0; i < orderItems.Count; i++)
+            {
+                allPrice += orderItems[i].UnitPrice;
+            }
+            return allPrice;
+        }
+    }
+}
